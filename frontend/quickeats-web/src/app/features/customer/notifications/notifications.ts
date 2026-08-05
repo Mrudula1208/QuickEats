4bade0bc33c1fb7fefd988c160c63b9a6c0f977a
Mrudula <moremrudula7@gmail.com>
2026-08-05 09:14:20 +0530

    Integrated Notification Module with Backend

--- frontend/quickeats-web/src/app/features/customer/notifications/notifications.ts
+++ frontend/quickeats-web/src/app/features/customer/notifications/notifications.ts
@@ -1,59 +1,58 @@
-// import { Component } from '@angular/core';
-// // Import Component because this file controls Notification Page.
+import { Component } from '@angular/core';
+// Import Component because this file controls Notification Page.
 
-// import { CommonModule } from '@angular/common';
-// // Import CommonModule because HTML uses @if and @for.
+import { CommonModule } from '@angular/common';
+// Import CommonModule because HTML uses @if and @for.
 
-// import { NotificationService } from '../../../core/services/notification.service';
-// // Import NotificationService because all notification logic is written there.
+import { NotificationService } from '../../../core/services/notification.service';
+// Import NotificationService because all notification logic is written there.
 
-// import { NotificationModel } from '../../../core/models/notification.model';
-// // Import NotificationModel because it defines one notification object.
+import { NotificationModel } from '../../../core/models/notification.model';
+// Import NotificationModel because it defines one notification object.
 
-// @Component({
-//   selector: 'app-notifications',
-//   standalone: true,
-//   imports: [
-//     CommonModule
-//   ],
-//   templateUrl: './notifications.html',
-//   styleUrl: './notifications.scss'
-// })
+@Component({
+  selector: 'app-notifications',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './notifications.html',
+  styleUrl: './notifications.scss'
+})
 
-// export class NotificationsComponent {
+export class NotificationsComponent {
 
-//   // Store all notifications.
-//   notifications: NotificationModel[] = [];
+  // Store all notifications.
+  notifications: NotificationModel[] = [];
 
-//   constructor(
+  constructor(
 
-//     // Angular automatically injects NotificationService.
-//     private notificationService: NotificationService
+    // Angular automatically injects NotificationService.
+    private notificationService: NotificationService
 
-//   ) {
+  ) {
 
-//     // Load notifications immediately.
-//     this.loadNotifications();
+    // Load notifications immediately.
+    this.loadNotifications();
 
-//   }
+  }
 
-//   // Load all notifications.
-//   loadNotifications(): void {
+  // Load all notifications.
+  loadNotifications(): void {
+      this.notificationService.getNotifications().subscribe({
+        next:(data:NotificationModel[])=> {
+          this.notifications = data;
+          console.log(this.notifications);
+        }
+    ,
+  error:(err:any)=> {
+    console.error( err);
+  
 
-//     this.notifications =
-//       this.notificationService.getNotifications();
+  // Runs when customer opens notification.
 
-//     console.log(this.notifications);
 
-//   }
 
-//   // Runs when customer opens notification.
-//   markAsRead(notificationId: number): void {
-
-//     this.notificationService.markAsRead(notificationId);
-
-//     this.loadNotifications();
-
-//   }
-
-// }
+      }});
+  }
+}
